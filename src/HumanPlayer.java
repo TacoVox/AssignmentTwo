b1e97938295a040e2f16f18962c38a95403c93f1
vryashkov <ivo.vryashkov@gmail.com>
2014-09-22 19:54:41 +0200

    testing

--- src/HumanPlayer.java
+++ src/HumanPlayer.java
@@ -3,6 +3,7 @@
  *  board for the
  *  second assignment in DIT948, 2014 edition.
  *  HumanPlayer implements the Player interface.
+ *  +Trying to see if this works // added comment
  */
 public class HumanPlayer implements Player {
     /**
@@ -16,4 +17,3 @@ public class HumanPlayer implements Player {
     }
 }
 
-// testing
