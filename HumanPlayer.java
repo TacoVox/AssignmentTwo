de3189c02339bc3837cc2be3ac72e85b01b5973a
jonas <jonas@dhcp-077216.eduroam.chalmers.se>
2014-09-25 14:25:42 +0200

    Added ask() function. Startet coding.

--- HumanPlayer.java
+++ HumanPlayer.java
@@ -3,9 +3,15 @@
  *  board for the
  *  second assignment in DIT948, 2014 edition.
  *  HumanPlayer implements the Player interface.
- *  + lets see if this works	// added
- */
-public class HumanPlayer implements Player {
+ **/
+
+//RESPONSIBILITY: Jonas Kahler
+
+//Import SimpleIO as provided by Cezar
+import static dit948.SimpleIO.*;
+
+public class HumanPlayer implements Player
+{
     /**
      *  Retrieve a row number and a column
      *  number from the user (which can be 0, 1, or 2)
@@ -13,7 +19,38 @@ public class HumanPlayer implements Player {
      *  occupy it.  For behavior on incorrect input,
      *  see the example session in the assignment text.
      */
-    public void move(TTTBoard board) {
+    public void move(TTTBoard board)
+    {
+    }
+    
+    //This function askss the user for the row. It returns an integer. 
+    private int ask(char type)
+    {
+    	//temporary input variable
+    	int usrinput;
+    	
+    	//Output to the user related if its a row or column
+    	if(type = 'r')
+    		println("Please enter a row:");
+    	else
+    		println("Please enter a column:");
+    	
+		//Assing usrinput with the input of the user
+    	usrinput = readInt();
+    	
+    	//While the input from the user is an illegal move or an IOError
+    	while(usrinput < 1 || usrinput usrinput > 3 || IOError())
+    	{
+    		//Output --> Information that the move isn't correct
+    		println("Your input isn't correct!");
+    		println("Please enter a number between 1 and 3:");
+    		
+    		//Assign usrinput again with the input of the user
+    		usrinput = readInt();
+    	}
+    	
+    	//Return the entered number
+    	return usrinput;
     }
 }
 
