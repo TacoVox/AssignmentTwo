3e81a7cad90cf6237b21f8e4bc6a4780fb907b07
vryashkov <ivo.vryashkov@gmail.com>
2014-09-26 12:11:29 +0200

    TTT.java

--- TTT.java
+++ TTT.java
@@ -1,8 +1,14 @@
+import static dit948.SimpleIO.*;
+
+
 /**
  *  Class TTT implements the necessary machinery to play one
  *  game of tic-tac-toe between two players, for the
  *  second assignment in DIT948, 2014 edition. //addedcomment
  */
+
+//	Responsible: Ivo Vryashkov
+
 public class TTT {
     /**
      *  A two-element array for the players.
@@ -17,7 +23,15 @@ public class TTT {
      *  Constructs a new game with the two players.
      */
     public TTT(Player player1, Player player2) {
-    }
+    	
+    	// assign players to array
+    	
+    	for (int i=0; i<players.length; i++) {
+    		players[0] = player1;
+    		players[1] = player2;
+    	} // end for loop
+    	
+    } // end public TTT
 
     /**
      *  Prints the index (0 or 1) of the player who has
@@ -28,7 +42,49 @@ public class TTT {
      *  The loop is exited when the game is over.
      */
     public void play() {
-    }
+    	
+    	// random integer for which player to move first
+    	int player = randomInt(2);
+    	
+    	// print which player is to move
+    	println(" Player " + player + " to move.");
+    	
+    	// print board (initially empty)
+    	println(board.toString());
+    	
+    	   	
+    	// do..while loop for the game (we want to run it at least one)
+    	// ends if game is over
+    	
+    	do{
+    		
+    		// integer for switching players
+    		int currentPlayer = player;
+    		
+    		// if condition for right player to move based on random number above
+    		if (currentPlayer == 0) {
+    			players[0].move();
+    		} else {
+    			players[1].move();
+    		} // end if condition
+    		
+    		// if condition to change players	
+    		if (currentPlayer == 0) {
+    			currentPlayer = 1;
+    		} else {
+    			currentPlayer = 0;
+    		} // end if condition
+    		
+    		// print who is next to move
+    		println("Player " + currentPlayer + " to move.");
+    		
+    		// print the board
+    		println(board.toString());
+    		
+    		
+    	}while (!gameOver);	// end do...while loop
+    	
+    }	//	end play()
 
     /**
      *  This is the main function which will be used
